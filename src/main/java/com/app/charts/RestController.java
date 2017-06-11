fac884b780f92860cf2103447b080ad571149725
schittimilla <schittimilla@ebay.com>
2017-06-11 15:35:29 -0700

    Charts initial commit

---
+++ src/main/java/com/app/charts/RestController.java
@@ -0,0 +1,12 @@
+package com.app.charts;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @RequestMapping("/api/hello")
+    public String greet() {
+        return "Hello from the other side !!!";
+    }
+}
+
